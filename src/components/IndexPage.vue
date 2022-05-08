8b79ed83c564e4b8649fc84e862cad02f8df6cb8
nevikw39 <nevikw39@icloud.com>
2022-05-08 23:24:43 +0800

    Add background in IndexPage

--- src/components/IndexPage.vue
+++ src/components/IndexPage.vue
@@ -1,21 +1,23 @@
 <template>
   <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">2022 IONCamp</h1>
-        <h2 class="subheading font-weight-regular">清大暑期程式競賽集訓營</h2>
-        <p class="subheading font-weight-regular">{{ brief }}</p>
-      </v-col>
-    </v-row>
-    <v-row class="text-center">
+    <v-parallax dark :src="require('../assets/bg.jpg')">
+      <v-row class="text-center">
+        <v-col cols="12">
+          <v-img
+            :src="require('../assets/logo.svg')"
+            class="my-3"
+            contain
+            height="200"
+          />
+        </v-col>
+        <v-col class="mb-4">
+          <h1 class="display-2 font-weight-bold mb-3">2022 IONCamp</h1>
+          <h2 class="subheading font-weight-regular">清大暑期程式競賽集訓營</h2>
+          <p class="subheading font-weight-regular">{{ brief }}</p>
+        </v-col>
+      </v-row>
+    </v-parallax>
+    <v-row class="text-center mt-3">
       <v-col>
         <v-card elevation="2" color="accent">
           <v-card-title>時程</v-card-title>
