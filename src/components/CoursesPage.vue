a8d11f5c7268522e7b807e38dfed9c5f3075cfab
nevikw39 <nevikw39@icloud.com>
2022-05-09 21:00:52 +0800

    Update courses

--- src/components/CoursesPage.vue
+++ src/components/CoursesPage.vue
@@ -13,7 +13,7 @@
           start="2022-07-25"
           end="2022-07-29"
           first-interval="8"
-          interval-count="10"
+          interval-count="15"
           :events="events"
           :event-color="getEventColor"
         ></v-calendar>
@@ -47,6 +47,12 @@ export default {
         color: "accent",
       },
       {
+        name: "交流討論時間",
+        start: "2022-07-25 19:00",
+        end: "2022-07-25 22:00",
+        color: "info",
+      },
+      {
         name: "進階資料結構",
         start: "2022-07-26 09:00",
         end: "2022-07-26 12:00",
@@ -57,6 +63,18 @@ export default {
         start: "2022-07-26 13:30",
         end: "2022-07-26 17:30",
         color: "accent",
+      },
+      {
+        name: "上機練習",
+        start: "2022-07-26 19:00",
+        end: "2022-07-26 21:30",
+        color: "success",
+      },
+      {
+        name: "題解",
+        start: "2022-07-26 21:30",
+        end: "2022-07-26 22:00",
+        color: "info",
       },
       {
         name: "進階 DP",
@@ -71,6 +89,18 @@ export default {
         color: "accent",
       },
       {
+        name: "上機練習",
+        start: "2022-07-27 19:00",
+        end: "2022-07-27 21:30",
+        color: "success",
+      },
+      {
+        name: "題解",
+        start: "2022-07-27 21:30",
+        end: "2022-07-27 22:00",
+        color: "info",
+      },
+      {
         name: "進階圖論",
         start: "2022-07-28 09:00",
         end: "2022-07-28 12:00",
@@ -83,6 +113,18 @@ export default {
         color: "accent",
       },
       {
+        name: "上機練習",
+        start: "2022-07-28 19:00",
+        end: "2022-07-28 21:30",
+        color: "success",
+      },
+      {
+        name: "題解",
+        start: "2022-07-28 21:30",
+        end: "2022-07-28 22:00",
+        color: "info",
+      },
+      {
         name: "其他解題技巧",
         start: "2022-07-29 09:00",
         end: "2022-07-29 12:00",
@@ -93,6 +135,12 @@ export default {
         start: "2022-07-29 13:30",
         end: "2022-07-29 17:30",
         color: "success",
+      },
+      {
+        name: "頒獎暨結業",
+        start: "2022-07-29 18:00",
+        end: "2022-07-29 19:00",
+        color: "error",
       },
     ],
   }),
